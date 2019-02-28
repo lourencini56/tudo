1d33dc0db2671f42f23e322fd9f4135afd290fe5
ALUNO <willianinformatica2018@gmail.com>
2019-02-28 15:44:27 -0300

    first

---
+++ database/migrations/2019_02_21_185152_create_produtos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProdutosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produtos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('Descricao');
+            $table->decimal('Preco',7,2);
+            $table->string('Cor');
+            $table->decimal('Peso',4,2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produtos');
+    }
+}
